0f6fcddc2492414fc827f4846c96fe0a9143eaae
Pankaj Rai <pankajvitmca@gmail.com>
2021-09-07 16:48:32 +0530

    delete button added and their style as well

--- video-3(Delete Records From Database)/index.php
+++ video-3(Delete Records From Database)/index.php
@@ -5,8 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Insert Data through Ajax</title>
+    <title>Delete Data through Ajax</title>
     <style>
+        .delete-btn {
+            background-color: red;
+            color: white;
+            border: 0;
+            padding: 4px 10px;
+            border-radius: 3px;
+        }
         #success-message {
             background: #DEF1D8;
             color: green;
