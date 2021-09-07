8858bd095eda9197a54e0df1569da6617bba298c
Pankaj Rai <pankajvitmca@gmail.com>
2021-09-07 18:14:38 +0530

    delete operation in Ajax through PHP

---
+++ video-3(Delete Records From Database)/ajax-delete.php
@@ -0,0 +1,17 @@
+<?php
+$student_id = $_POST["id"];
+
+$conn = mysqli_connect("localhost", "root", "", "test") or die("Connection Failed");
+
+$sql = "delete from students where id = {$student_id}";
+
+if(mysqli_query($conn, $sql)) {
+    echo 1;
+} else {
+    echo 0;
+}
+
+
+
+
+?>
--- video-3(Delete Records From Database)/index.php
+++ video-3(Delete Records From Database)/index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Delete Data through Ajax</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
         .delete-btn {
             background-color: red;
@@ -13,7 +14,9 @@
             border: 0;
             padding: 4px 10px;
             border-radius: 3px;
+            cursor: pointer;
         }
+
         #success-message {
             background: #DEF1D8;
             color: green;
@@ -24,6 +27,7 @@
             right: 15px;
             top: 15px;
         }
+
         #error-message {
             background: #EFDCDD;
             color: red;
@@ -62,6 +66,9 @@
     <div id="error-message"></div>
     <div id="success-message"></div>
     <script type="text/javascript" src="js/jquery.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function() {
             function loadTable() {
@@ -102,7 +109,29 @@
                         }
                     });
                 }
-            })
+            });
+
+            $(document).on("click", ".delete-btn", function() {
+                if (confirm("Do you really want to delete this record ?")) {
+                    var studentId = $(this).data("id");
+                    var element = this;
+                    $.ajax({
+                        url: "ajax-delete.php",
+                        type: "POST",
+                        data: {
+                            id: studentId
+                        },
+                        success: function(data) {
+                            if (data) {
+                                $(element).closest("tr").fadeOut();
+                            } else {
+                                $("#error-message").html("Can't Delete record").slideDown();
+                                $("#success-message").slideUp();
+                            }
+                        }
+                    });
+                }
+            });
         });
     </script>
 </body>
